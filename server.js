f3a0c1e08cdb2cc0ae94862c2193527f2b26bf7d
Krishnan R <vinokrish001@gmail.com>
2023-04-06 18:38:08 +0530

    camera controlled remotely

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const TOPIC_NAME = 'imu-data';
+const subscribers = new Set();
+
+app.use(express.static(__dirname + ''));
+
+io.on('connection', (socket) => {
+  console.log(`a user connected: ${socket.id}`);
+  socket.on('publish', (data) => {
+    console.log(`received data from ${socket.id}: ${data}`);
+    const jsonData = JSON.parse(data);
+    io.emit(TOPIC_NAME, jsonData);
+  });
+  socket.on('subscribe', () => {
+    console.log(`subscribed user: ${socket.id}`);
+    subscribers.add(socket);
+  });
+  socket.on('unsubscribe', () => {
+    console.log(`unsubscribed user: ${socket.id}`);
+    subscribers.delete(socket);
+  });
+});
+
+http.listen(5500, () => {
+  console.log('listening on *:3000');
+});
